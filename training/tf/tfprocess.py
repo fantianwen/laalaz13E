e8c8b20f4d5bea894e4e15464edbdabb3788a248
fantianwen <fantianwen121@gmail.com>
2019-04-22 18:42:42 +0900

    13

--- training/tf/tfprocess.py
+++ training/tf/tfprocess.py
@@ -155,8 +155,8 @@ class TFProcess:
 
         planes = tf.to_float(planes)
 
-        planes = tf.reshape(planes, (batch_size, 18, 19*19))
-        probs = tf.reshape(probs, (batch_size, 19*19 + 1))
+        planes = tf.reshape(planes, (batch_size, 18, 13*13))
+        probs = tf.reshape(probs, (batch_size, 13*13 + 1))
         winner = tf.reshape(winner, (batch_size, 1))
 
         if gpus_num is None:
@@ -583,9 +583,9 @@ class TFProcess:
                                    input_channels=self.RESIDUAL_FILTERS,
                                    output_channels=2,
                                    name="policy_head")
-        h_conv_pol_flat = tf.reshape(conv_pol, [-1, 2 * 19 * 19])
-        W_fc1 = weight_variable("w_fc_1", [2 * 19 * 19, (19 * 19) + 1])
-        b_fc1 = bias_variable("b_fc_1", [(19 * 19) + 1])
+        h_conv_pol_flat = tf.reshape(conv_pol, [-1, 2 * 13 * 13])
+        W_fc1 = weight_variable("w_fc_1", [2 * 13 * 13, (13 * 13) + 1])
+        b_fc1 = bias_variable("b_fc_1", [(13 * 13) + 1])
         self.add_weights(W_fc1)
         self.add_weights(b_fc1)
         h_fc1 = tf.add(tf.matmul(h_conv_pol_flat, W_fc1), b_fc1)
@@ -595,8 +595,8 @@ class TFProcess:
                                    input_channels=self.RESIDUAL_FILTERS,
                                    output_channels=1,
                                    name="value_head")
-        h_conv_val_flat = tf.reshape(conv_val, [-1, 19 * 19])
-        W_fc2 = weight_variable("w_fc_2", [19 * 19, 256])
+        h_conv_val_flat = tf.reshape(conv_val, [-1, 13 * 13])
+        W_fc2 = weight_variable("w_fc_2", [13 * 13, 256])
         b_fc2 = bias_variable("b_fc_2", [256])
         self.add_weights(W_fc2)
         self.add_weights(b_fc2)
@@ -686,11 +686,11 @@ class TFProcessTest(unittest.TestCase):
                 tfprocess.RESIDUAL_FILTERS, tfprocess.RESIDUAL_FILTERS))
         # policy
         data.extend(gen_block(1, tfprocess.RESIDUAL_FILTERS, 2))
-        data.append([0.4] * 2*19*19 * (19*19+1))
-        data.append([0.5] * (19*19+1))
+        data.append([0.4] * 2*13*13 * (13*13+1))
+        data.append([0.5] * (13*13+1))
         # value
         data.extend(gen_block(1, tfprocess.RESIDUAL_FILTERS, 1))
-        data.append([0.6] * 19*19 * 256)
+        data.append([0.6] * 13*13 * 256)
         data.append([0.7] * 256)
         data.append([0.8] * 256)
         data.append([0.9] * 1)
